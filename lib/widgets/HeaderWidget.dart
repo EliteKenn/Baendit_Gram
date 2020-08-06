3bcf16bfadfaa9e657bbc0185ce3d69ef1f677b2
Kennedy <argosax2@yahoo.com>
2020-08-06 00:06:23 -0400

    Initial commit

---
+++ lib/widgets/HeaderWidget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+AppBar header(context, {bool isAppTitle = false, String strTitle, dissapearBackButton = false}) {
+  return AppBar(
+    iconTheme: IconThemeData(
+      color: Colors.white,
+
+    ),
+    automaticallyImplyLeading: dissapearBackButton ? false : true,
+    title: Text(
+      isAppTitle ? "BaenditGram" : strTitle,
+      style: TextStyle(
+        color: Colors.white,
+        fontFamily: isAppTitle ? "Signatra" : "",
+        fontSize: isAppTitle ? 45.0 : 22.0,
+      ),
+      overflow: TextOverflow.ellipsis,
+    ),
+    centerTitle: true,
+    backgroundColor: Theme.of(context).accentColor,
+  );
+}
